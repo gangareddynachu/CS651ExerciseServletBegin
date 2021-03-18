fd4c8ef764b23e26c56717a54e97758c8f67bfcc
gangareddynachu <gnachu@horizon.csueastbay.edu>
2021-03-18 14:09:45 -0700

    First Commit

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet({"/Hello","/" })
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+	private String message = "";
+	
+    public HelloWorldServlet() {
+        // TODO Auto-generated constructor stub
+    	message = "Hello World";
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		
+		out.println(message);
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
